a934776f26c7c9f868d26bba756ad374761b9bc3
XCH-Gustavo <xchoramhunter@gmail.com>
2022-04-29 01:30:57 -0300

    Jogo de nave até parte 6

---
+++ jogo_de_nave/js/js.js
@@ -0,0 +1,29 @@
+function start() { // Inicio da função start()
+
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    var jogo = {}
+
+    //Game Loop
+
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+
+        movefundo();
+
+    } // Fim da função loop()
+
+    function movefundo() {
+
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 1);
+
+    } // fim da função movefundo()
+
+} // Fim da função start
